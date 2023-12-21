0bde40df2947e2e7e26ad01c89387f0245f9f7ee
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2023-12-21 15:05:34 +0200

    Create findSysAdminUsers.sql

---
+++ security/findSysAdminUsers.sql
@@ -0,0 +1,10 @@
+-- find all sysadmins in the system 
+
+SELECT DISTINCT su.systemuserid, su.fullname
+  FROM systemuser su
+  JOIN systemuserroles sur
+    ON su.systemuserid = sur.systemuserid
+  JOIN role r
+    ON r.roleid = sur.roleid
+ WHERE r.name = 'System Administrator'
+   AND su.applicationid IS NULL -- remove Application User from this selection
