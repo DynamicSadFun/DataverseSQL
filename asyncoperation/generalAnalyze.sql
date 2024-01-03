c77289effd7c4438a9add22c2622ddfe909c8035
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2024-01-03 11:24:35 +0200

    Create generalAnalyze.sql

---
+++ asyncoperation/generalAnalyze.sql
@@ -0,0 +1,17 @@
+--In the event of an overflow of Capacity and 
+--in particular your AsyncOperation table, 
+--this query will show a preliminary result 
+--for which data can be painlessly deleted from the system
+
+SELECT operationtypename, statecodename, COUNT(AsyncOperationId)
+  FROM AsyncOperation
+    --1	    System Event
+    --9	    Collect SQM data
+    --10    Workflow Operation
+    --12	Update Match Code
+    --25	Generate Full Text Catalog
+    --27	Update Contract States
+ WHERE OperationType IN (1, 9, 10, 12, 25, 27) 
+   AND StateCode = 3 -- Completed
+   AND StatusCode IN (30, 32)   -- Succeded and Canceled 
+ GROUP BY operationtypename, statecodename
