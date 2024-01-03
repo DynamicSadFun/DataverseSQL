027474fa3fd6934743ec85b7ffb9a46aad4bbd50
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2024-01-03 11:30:26 +0200

    Create freqInteraction.sql

---
+++ contact/freqInteraction.sql
@@ -0,0 +1,11 @@
+--This query helps to understand how frequently you're interacting with contacts by counting activities associated with each contact over the months.
+
+SELECT c.contactid, c.fullname, a.activitytypecodename,
+       MONTH(a.createdon) AS InteractionMonth, 
+       YEAR(a.createdon) AS InteractionYear, 
+       COUNT(*) AS InteractionCount
+  FROM contact c
+  JOIN activitypointer a 
+    ON c.contactid = a.regardingobjectid
+ GROUP BY c.contactid, c.fullname, a.activitytypecodename, MONTH(a.createdon), YEAR(a.createdon)
+ ORDER BY InteractionYear, InteractionMonth, c.fullname, InteractionCount DESC;
