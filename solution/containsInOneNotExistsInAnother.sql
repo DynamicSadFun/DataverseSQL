d5f9c5eea5b1e6a8153b4a1992f8b7324441b16b
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2024-01-29 22:16:34 +0200

    Create containsInOneNotExistsInAnother.sql

---
+++ solution/containsInOneNotExistsInAnother.sql
@@ -0,0 +1,17 @@
+--This query will show components that are contained in one solution but are not contained in another.
+--A kind of reconciliation to avoid incorrect Solution Layering. 
+
+SELECT DISTINCT sc1.objectid, sc1.componenttypename
+  FROM solution s1
+  JOIN solutioncomponent sc1
+    ON s1.solutionid = sc1.solutionid
+ WHERE s1.friendlyname = 'YOUR_SOLUTION_NAME_#1'
+   AND NOT EXISTS
+(
+    SELECT DISTINCT sc.objectid
+      FROM solution s
+      JOIN solutioncomponent sc
+        ON s.solutionid = sc.solutionid
+     WHERE s.friendlyname = 'YOUR_SOLUTION_NAME_#2'
+       AND sc.objectid = sc1.objectid
+)
