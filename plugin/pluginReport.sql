7738f7d8b661a8a02fff26268e4724c591b11b2b
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2024-02-05 14:57:26 +0200

    Create pluginReport.sql

---
+++ plugin/pluginReport.sql
@@ -0,0 +1,11 @@
+-- find our plufins with modename (Sync / Async) and autodelete logs flag
+
+SELECT DISTINCT asyncautodeletename, plugin.modename, plugin.name, solution.friendlyname
+  FROM sdkmessageprocessingstep plugin WITH(NOLOCK)
+  JOIN solutioncomponent sc WITH(NOLOCK)
+    ON sc.objectid = plugin.sdkmessageprocessingstepid
+  JOIN solution WITH(NOLOCK)
+    ON sc.solutionid = solution.solutionid
+-- put your solution or publisher name into condition
+ WHERE solution.friendlyname = 'YOUR_SOLUTION_NAME'
+    OR solution.publisheridname = 'YOUR_PUBLISHER_NAME'
