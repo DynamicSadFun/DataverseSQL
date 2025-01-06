9452b6068785a0bcbca80f89d0115b7ff214429c
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-01-06 12:54:29 +0200

    Update getUnmanagedComponents.sql

--- solution/getUnmanagedComponents.sql
+++ solution/getUnmanagedComponents.sql
@@ -1,31 +1,46 @@
 --We are trying to find all unmanaged components that are contained in a managed solution, but were also published in unmanaged solutions. 
 
-SELECT DISTINCT 
-       s.friendlyname                   AS [Unmanaged Solution Name], 
-       s.createdon                      AS [Created On], 
-       s.createdbyname                  AS [Created By],  
-       sc.objectid                      AS [Unmanaged Component ID],
-       sc.componenttype                 AS [Unmanaged Component],
-       CASE sc.componenttype
-        WHEN 80 THEN 'App Module'
-        WHEN 10430 THEN 'Connection Reference'
-        ELSE sc.componenttypename     
-       END                              AS [Unmanaged Component Type],
-       --managed.objectid                 AS [Managed Component ID],
-       managed.friendlyname             AS [This Component Contains In Managed Solution Name]
-  FROM solution s
-  JOIN solutioncomponent sc 
-    ON s.solutionid = sc.solutionid
-  LEFT JOIN (
-    SELECT manSol.friendlyname, manComp.objectid
+WITH ManagedSolutions AS (
+    SELECT manComp.objectid, manSol.friendlyname
       FROM solutioncomponent manComp
       JOIN solution manSol
         ON manSol.solutionid = manComp.solutionid
-     WHERE manSol.ismanaged = 1
+       AND manSol.ismanaged = 1
        AND manSol.isvisible = 1
-   ) AS managed
-    ON managed.objectid = sc.objectid
- WHERE s.ismanaged = 0
-   AND s.isvisible = 1
-   AND s.friendlyname NOT IN ('Active Solution', 'Default Solution', 'Common Data Services Default Solution')
- ORDER BY s.createdon
+),
+UnmanagedComponents AS (
+    SELECT DISTINCT
+           s.friendlyname     AS [Unmanaged Solution Name],
+           s.createdon        AS [Created On],
+           s.createdbyname    AS [Created By],
+           sc.objectid        AS [Unmanaged Component ID],
+           sc.componenttype   AS [Unmanaged Component Type],
+           ISNULL
+           (
+             CASE sc.componenttype
+               WHEN 80 THEN 'App Module'
+               WHEN 10430 THEN 'Connection Reference'
+               ELSE sc.componenttypename   
+             END, 
+             'Other Component (please check the solution)'
+           )                    AS [Unmanaged Component Name],
+           managed.friendlyname AS [Contained in Managed Solution Name]
+      FROM solution s
+      JOIN solutioncomponent sc
+        ON s.solutionid = sc.solutionid
+      LEFT JOIN ManagedSolutions managed
+        ON managed.objectid = sc.objectid
+     WHERE s.ismanaged = 0
+       AND s.isvisible = 1
+       AND s.friendlyname NOT IN ('Active Solution', 'Default Solution', 'Common Data Services Default Solution')
+)
+
+SELECT [Unmanaged Solution Name], 
+       [Created On], 
+       [Created By], 
+       [Unmanaged Component ID], 
+       [Unmanaged Component Type],
+       [Unmanaged Component Name],
+       [Contained in Managed Solution Name]
+  FROM UnmanagedComponents
+ ORDER BY [Created On];
