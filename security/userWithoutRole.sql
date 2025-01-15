4f629786a4fc359b17fd19fb1fb852f92c33ebd7
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-01-15 13:07:36 +0200

    Update userWithoutRole.sql

--- security/userWithoutRole.sql
+++ security/userWithoutRole.sql
@@ -1,9 +1,13 @@
 --This helps in auditing and ensuring all users have appropriate access and roles.
 
-SELECT DISTINCT su.systemuserid, su.fullname 
+SELECT DISTINCT 
+       su.systemuserid, 
+       su.fullname, 
+       IIF(sur.roleid IS NOT NULL, 'User has some role', 'No roles') AS result
   FROM systemuser su  
   LEFT JOIN systemuserroles sur 
     ON su.systemuserid = sur.systemuserid 
- WHERE sur.systemuserid IS NULL 
-   AND su.isdisabled = 0 -- Assuming isdisabled flag is 0 for enabled users
+ WHERE su.applicationid IS NULL -- remove Application User from this selection
+   AND su.isdisabled = 0        -- active users
+   AND su.accessmode = 0        -- read-write users (it means physical person)
  ORDER BY 2
