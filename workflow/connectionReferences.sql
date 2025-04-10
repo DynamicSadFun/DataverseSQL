6788db5ad5b76e5d61d967196ca15b966a933432
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-04-10 15:54:02 +0300

    Update connectionReferences.sql

--- workflow/connectionReferences.sql
+++ workflow/connectionReferences.sql
@@ -1,6 +1,6 @@
 -- this query parses the contents of your PA flows, 
 -- extracts connectors from them and groups your flows by them, 
--- counting how many flows belong to a particular connector.
+-- counting how many flows belong to a particular connector
 WITH JsonData AS (
     -- Extract the connection references from the JSON clientdata field
     SELECT w.name AS FlowName, 
@@ -11,10 +11,9 @@ WITH JsonData AS (
        AND w.statecode = 1 -- only active flows
 )
 -- Group the results by ConnectorName and count the number of flows for each connector
-SELECT ConnectorName, 
-       COUNT(FlowName) AS FlowCount
+SELECT REPLACE(ConnectorName, 'shared_', '')    AS ConnectorName, 
+       COUNT(FlowName)                          AS FlowCount
   FROM JsonData
  WHERE ConnectorName IS NOT NULL
  GROUP BY ConnectorName
  ORDER BY FlowCount DESC;
- 
