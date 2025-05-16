0187f327bab6e16bdfc8f7f4dc8c4465ef0e4938
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-05-16 16:41:24 +0300

    Update environmentvariables.sql

--- environmentvariables/environmentvariables.sql
+++ environmentvariables/environmentvariables.sql
@@ -1,18 +1,15 @@
 -- List all Environment Variables (Definitions and their active values)
 SELECT DISTINCT
-    evd.environmentvariabledefinitionid AS DefinitionId,
-    evd.schemaname AS SchemaName,
-    evd.displayname AS DisplayName,
-    evd.defaultvalue AS DefaultValue,
-    evv.value AS CurrentValue,
-    evd.type AS DataType,
-    evd.statecode AS StateCode,
-    evv.createdon AS ValueCreatedOn,
-    evv.modifiedon AS ValueModifiedOn
-FROM 
-    environmentvariabledefinition evd
-LEFT JOIN 
-    environmentvariablevalue evv
+       evd.environmentvariabledefinitionid AS DefinitionId,
+       evd.schemaname                      AS SchemaName,
+       evd.displayname                     AS DisplayName,
+       evd.defaultvalue                    AS DefaultValue,
+       evv.value                           AS CurrentValue,
+       evd.type                            AS DataType,
+       evd.statecode                       AS StateCode,
+       evv.createdon                       AS ValueCreatedOn,
+       evv.modifiedon                      AS ValueModifiedOn
+  FROM environmentvariabledefinition evd
+  LEFT JOIN environmentvariablevalue evv
     ON evd.environmentvariabledefinitionid = evv.environmentvariabledefinitionid
-ORDER BY 
-    evd.displayname;
+ ORDER BY evd.displayname
