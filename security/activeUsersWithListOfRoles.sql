a6404b6b71ae10d48f0118d3279b47941f34386a
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-06-17 10:18:58 +0200

    Update activeUsersWithListOfRoles.sql

--- security/activeUsersWithListOfRoles.sql
+++ security/activeUsersWithListOfRoles.sql
@@ -2,24 +2,26 @@
 --and the list of roles itself be presented in a single cell for convenience 
 SELECT DISTINCT 
        su.systemuserid,
-	     su.fullname, 
-	     su.domainname,
-       ISNULL(STUFF(
-					(SELECT ', ' + r.name
-					   FROM systemuserroles sur WITH(NOLOCK)
-					   JOIN role r WITH(NOLOCK)
-					     ON sur.roleid = r.roleid
-					  WHERE sur.systemuserid = su.systemuserid
-					  ORDER BY r.name
-					    FOR XML PATH('')), 1, 2, ''
-				   ), 
-			 'No Roles'
-	     ) AS roles
+       su.fullname, 
+       su.domainname,
+       ISNULL(
+	  STUFF(
+		(
+		 SELECT ', ' + r.name
+		   FROM systemuserroles sur WITH(NOLOCK)
+		   JOIN role r WITH(NOLOCK)
+		     ON sur.roleid = r.roleid
+		  WHERE sur.systemuserid = su.systemuserid
+		  ORDER BY r.name
+		    FOR XML PATH('')
+		), 1, 2, ''
+	  ), 'No Roles'
+       ) AS roles
   FROM systemuser su
   JOIN systemuserroles surr WITH(NOLOCK)
     ON su.systemuserid = surr.systemuserid
   JOIN role rr WITH(NOLOCK)
-	  ON surr.roleid = rr.roleid
+    ON surr.roleid = rr.roleid
  WHERE su.applicationid IS NULL -- remove Application User from this selection
    AND su.isdisabled = 0 -- active users
    AND su.accessmode = 0 -- read-write users (it means physical person)
