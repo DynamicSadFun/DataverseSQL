fc00ec5b8bf0f47eb05eb43857cb961737200980
Valentin Gasenko <86048404+DynamicSadFun@users.noreply.github.com>
2025-06-17 10:16:43 +0200

    Update webFilesNotes.sql

--- powerpages/webFilesNotes.sql
+++ powerpages/webFilesNotes.sql
@@ -1,7 +1,8 @@
 --as you know, there should be no more than one attachment under webfiles, 
 --but there are often collisions. 
 --This simple query will allow you to understand which webfiles have more than one attachment. 
-SELECT x.adx_name AS filename, COUNT(DISTINCT a.annotationid) AS count_of_notes
+SELECT x.adx_name                     AS filename, 
+       COUNT(DISTINCT a.annotationid) AS count_of_notes
   FROM annotation a
   JOIN adx_webfile x ON a.objectid = x.adx_webfileid
  WHERE x.statecode = 0
